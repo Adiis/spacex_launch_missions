c1481cab22f2d9024f256bf83dd9f49faf9a2307
Adis <adis.hasovic@gmail.com>
2018-11-25 14:45:50 +0100

    producation

---
+++ public/precache-manifest.206e6ac10384ee71f7918a5004061516.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6344d79ce01027344d64",
+    "url": "/static/css/main.d72e91b2.chunk.css"
+  },
+  {
+    "revision": "6344d79ce01027344d64",
+    "url": "/static/js/main.6344d79c.chunk.js"
+  },
+  {
+    "revision": "700791a65b6d6cf718c9",
+    "url": "/static/js/1.700791a6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3d1b739fb2546948f207d2be7ae1b105",
+    "url": "/static/media/spinner.3d1b739f.gif"
+  },
+  {
+    "revision": "f751c040a2fdbdda3010475ed128f86b",
+    "url": "/static/media/logo.f751c040.png"
+  },
+  {
+    "revision": "d4bb054b848b51c5395c9a8c09de8f5a",
+    "url": "/index.html"
+  }
+];
